b7c8945c079789dd3e99b0ff8d1d990ff7f73fb9
venu04143 <venu04143@gmail.com>
2023-11-22 00:03:50 +0530

    first commit

---
+++ generic/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def melisandre(request):
+    return render(request,'melisandre.html')
+
+def serdavos(request):
+    return render(request,'serdavos.html')
+
+def melisandre1(request):
+    return render(request,'melisandre1.html')
+
+def serdavos1(request):
+    return render(request,'serdavos1.html')
+
+def melisandre2(request):
+    return render(request,'melisandre2.html')
+
+def serdavos2(request):
+    return render(request,'serdavos2.html')
+
+def melisandre3(request):
+    return render(request,'melisandre3.html')
+
+def serdavos3(request):
+    return render(request,'serdavos3.html')
+
+def melisandre4(request):
+    return render(request,'melisandre4.html')
+
+def serdavos4(request):
+    return render(request,'serdavos4.html')
+
+def melisandre5(request):
+    return render(request,'melisandre5.html')
+
+def serdavos5(request):
+    return render(request,'serdavos5.html')
